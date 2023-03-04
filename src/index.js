3baeb06e7ae59e3326823be9efa4b2d6b7112df8
exit <saraswathi.mac@gmail.com>
2023-03-04 20:05:15 +0530

    Removed unnecessary files

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import reducer, {initialState} from "./google_pages/reducer";
 import { StateProvider } from "./google_pages/StateProvider";
 
@@ -14,5 +13,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
-
-reportWebVitals();
